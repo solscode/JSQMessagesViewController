66fba423c17ecb12bb95c7817964a7ba12a4b90d
solscode <37924732+solscode@users.noreply.github.com>
2018-04-17 17:38:39 +0900

    Update JSQMessagesCollectionViewCellIncoming.m

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
@@ -27,6 +27,7 @@
     [super awakeFromNib];
     self.messageBubbleTopLabel.textAlignment = NSTextAlignmentLeft;
     self.cellBottomLabel.textAlignment = NSTextAlignmentLeft;
+    self.cellBottomCountLabel.textAlignment = NSTextAlignmentCenter;
 }
 
 @end
